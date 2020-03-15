6c09204883285d9d4c41f4ec11c03df3196d4a58
KazuDante89 <karlemmanueldorvil@gmail.com>
2020-03-15 14:17:46 -0400

    needed for circleci

---
+++ anykernel.sh
@@ -0,0 +1,143 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=Phoenix_kernel by KazuDante
+do.devicecheck=1
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=lavender
+device.name2=
+device.name3=
+device.name4=
+device.name5=
+supported.versions=9 - 10
+supported.patchlevels=
+'; } # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+
+
+# Key select start
+
+INSTALLER=$(pwd)
+KEYCHECK=$INSTALLER/tools/keycheck
+chmod 755 $KEYCHECK
+
+keytest() {
+  (/system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > $INSTALLER/events) || return 1
+  return 0
+}
+
+choose() {
+  #note from chainfire @xda-developers: getevent behaves weird when piped, and busybox grep likes that even less than toolbox/toybox grep
+  while true; do
+    /system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > $INSTALLER/events
+    if (`cat $INSTALLER/events 2>/dev/null | /system/bin/grep VOLUME >/dev/null`); then
+      break
+    fi
+  done
+  if (`cat $INSTALLER/events 2>/dev/null | /system/bin/grep VOLUMEUP >/dev/null`); then
+    return 0
+  else
+    return 1
+  fi
+}
+
+chooseold() {
+  # Calling it first time detects previous input. Calling it second time will do what we want
+  $KEYCHECK
+  $KEYCHECK
+  SEL=$?
+  if [ "$1" == "UP" ]; then
+    UP=$SEL
+  elif [ "$1" == "DOWN" ]; then
+    DOWN=$SEL
+  elif [ $SEL -eq $UP ]; then
+    return 0
+  elif [ $SEL -eq $DOWN ]; then
+    return 1
+  else
+    ui_print "   Vol key not detected!"
+    abort "   Use name change method in TWRP"
+  fi
+}
+
+if [ -f /tmp/anykernel/dtbs/*.dtb-custom ]; then
+  if [ -z $NEW ]; then
+    if keytest; then
+      FUNCTION=choose
+    else
+      FUNCTION=chooseold
+      ui_print " "
+      ui_print "Swipe up the screen or press any volume key first"
+      ui_print " "
+      ui_print "- Vol Key Programming -"
+      ui_print "   Press Volume Up Key: "
+      $FUNCTION "UP"
+      ui_print "   Press Volume Down Key: "
+      $FUNCTION "DOWN"
+    fi
+    ui_print " "
+    ui_print "- Select Option -"
+    ui_print "   Choose the Big CPU clock rate you want:"
+    ui_print "   Volume Up = 1.95 GHz"
+    ui_print "   Volume Down = 2.2 GHz"
+    if $FUNCTION; then
+      NEW=true
+    else
+      NEW=false
+    fi
+  else
+    ui_print "   Option specified in zipname!"
+  fi
+else
+  NEW=false
+fi
+# Key select end
+
+
+## AnyKernel install
+dump_boot;
+
+
+# If the kernel image and dtbs are separated in the zip
+decompressed_image=/tmp/anykernel/kernel/Image
+compressed_image=$decompressed_image.gz
+if [ -f $compressed_image ]; then
+  # Hexpatch the kernel if Magisk is installed ('skip_initramfs' -> 'want_initramfs')
+  if [ -d $ramdisk/.backup ]; then
+    ui_print " "; ui_print "Magisk detected! Won't do you any good since your device is about to catch on fire...";
+    $bin/magiskboot --decompress $compressed_image $decompressed_image;
+    $bin/magiskboot --hexpatch $decompressed_image 736B69705F696E697472616D667300 77616E745F696E697472616D667300;
+    $bin/magiskboot --compress=gzip $decompressed_image $compressed_image;
+  fi;
+
+  # Concatenate all of the dtbs to the kernel
+  if $NEW; then
+    cat $compressed_image /tmp/anykernel/dtbs/*.dtb-custom > /tmp/anykernel/Image.gz-dtb;
+  else
+    cat $compressed_image /tmp/anykernel/dtbs/*.dtb > /tmp/anykernel/Image.gz-dtb;
+  fi
+fi;
+
+
+write_boot;
+## end install
